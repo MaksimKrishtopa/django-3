d5f59f9cd175bbcef426ed1804554462cc59c0dc
MaksimKrishtopa <k.maks21@mail.ru>
2023-11-22 18:24:51 +0700

    Приступили к выполнение 4 части, измнены views(Модернизируем маршруты приложения), изменение urls

--- tutor-practice/tutorial/snippets/views.py
+++ tutor-practice/tutorial/snippets/views.py
@@ -83,5 +83,39 @@ def snippet_detail(request, pk):
        snippet.delete()
        return Response(status=status.HTTP_204_NO_CONTENT)
 
+   def get(self, request, format=None):
+       snippets = Snippet.objects.all()
+       serializer = SnippetSerializer(snippets, many=True)
+       return Response(serializer.data)
 
+   def post(self, request, format=None):
+       serializer = SnippetSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+   def get_object(self, pk):
+       try:
+           return Snippet.objects.get(pk=pk)
+       except Snippet.DoesNotExist:
+           raise Http404
+
+   def get(self, request, pk, format=None):
+       snippet = self.get_object(pk)
+       serializer = SnippetSerializer(snippet)
+       return Response(serializer.data)
+
+   def put(self, request, pk, format=None):
+       snippet = self.get_object(pk)
+       serializer = SnippetSerializer(snippet, data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+   def delete(self, request, pk, format=None):
+       snippet = self.get_object(pk)
+       snippet.delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
 
